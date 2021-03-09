a6850a1b51c649c98c80c7a8f0ae91d910d3fa10
Sam Ziegler <szig97@outlook.com>
2021-03-09 16:28:26 -0600

    adding q7 analysis

--- SQL/employee_db(queries).sql
+++ SQL/employee_db(queries).sql
@@ -44,6 +44,13 @@ on dept_emp.dept_no = departments.dept_no
 where departments.dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+join employees
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales'
+or departments.dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
