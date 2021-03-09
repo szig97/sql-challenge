eaa847dc79eac0d31a4db08eabad91246bd3600a
Sam Ziegler <szig97@outlook.com>
2021-03-09 16:22:59 -0600

    adding q6 analysis

--- SQL/employee_db(queries).sql
+++ SQL/employee_db(queries).sql
@@ -35,7 +35,13 @@ where first_name = 'Hercules'
 and last_name like 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+join employees
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
