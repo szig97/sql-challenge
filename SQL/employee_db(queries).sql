d1e212d1593f0b9c03f37a12901172ed3d0b58be
Sam Ziegler <szig97@outlook.com>
2021-03-09 16:15:29 -0600

    adding q5 analysis

--- SQL/employee_db(queries).sql
+++ SQL/employee_db(queries).sql
@@ -29,7 +29,10 @@ join employees
 on dept_emp.emp_no = employees.emp_no;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+select first_name, last_name
+from employees
+where first_name = 'Hercules' 
+and last_name like 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
